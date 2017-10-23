933bf7aca8eb8ba8b2aec668f5d286c5f1cb3b5b
003BeletskyVA <003BeletskyVA@WS00303501005.0003.PFR.RU>
2017-10-23 13:19:46 +0800

    connect with MySQL

---
+++ DataMiner/Params.cs
@@ -0,0 +1,21 @@
+﻿namespace DataMiner
+{
+    public class Params
+    {
+        public string dbHost { get; set; }
+        public string dbDatabase { get; set; }
+        public string dbUser { get; set; }
+        public string dbPassword { get; set; }
+        public string dbProtocol { get; set; }
+        public string dbPort { get; set; }
+        public string msqlHost { get; set; }
+        public string msqlDatabase { get; set; }
+        public string msqlUser { get; set; }
+        public string msqlPassword { get; set; }
+        public string msqlProtocol { get; set; }
+        public string msqlPort { get; set; }
+        public long StartDate { get; set; }
+        public int Department { get; set; }
+        
+    }
+}
